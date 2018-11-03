5b068e960353d382de791719eff3e9eabaa173bf
stuhlmeier <stuhlmeier@gmail.com>
2018-11-03 11:00:50 +0100

    Reorganize folder structure

--- script/generate/draw.js
+++ script/generate/draw.js
@@ -1,4 +1,4 @@
-import * as util from './util.js'
+import * as util from '../util.js'
 
 export default function draw(bytes, generator) {
     const pageCanvas = document.getElementById("canvas");
--- script/generate/generators/bwGenerator.js
+++ script/generate/generators/bwGenerator.js
@@ -1,4 +1,4 @@
-import * as util from '../util.js'
+import * as util from '../../util.js'
 
 export default class BwGenerator {
 
