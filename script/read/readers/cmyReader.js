54683909823edd189ede14256418d1090a27bcd0
stuhlmeier <stuhlmeier@gmail.com>
2018-11-04 12:32:09 +0100

    Disable input until script has loaded

--- script/read/readers/cmyReader.js
+++ script/read/readers/cmyReader.js
@@ -5,13 +5,13 @@ export default class CmyReader {
     }
 
     process(buffer, r, g, b) {
-        if (r === 255 && g === 255 && b === 0) {
+        if (r === 255 && g === 255) {
             buffer.push(1);
             buffer.push(0);
-        } else if (r === 255 && g === 0 && b === 255) {
+        } else if (r === 255 && b === 255) {
             buffer.push(0);
             buffer.push(1);
-        } else if (r === 0 && g === 255 && b === 255) {
+        } else if (g === 255 && b === 255) {
             buffer.push(1);
             buffer.push(1);
         } else if (r === 0 && g === 0 && b === 0) {
